bef78ab5026343f1eeb9ffc481eb5235e5c93426
cslkn <164247228+cslkn@users.noreply.github.com>
2024-04-08 12:19:35 -0500

    fixed navbar

--- react-frontend/src/Navigation.js
+++ react-frontend/src/Navigation.js
@@ -7,13 +7,12 @@ function Navigation({ cartItemCount }) {
     return (
         <nav style={{ backgroundColor: 'black', color: 'white', display: 'flex', justifyContent: 'space-between', padding: '10px', fontSize: '1.2rem' }}>
             <div style={{ display: 'flex', alignItems: 'center' }}>
-                <img src={logo} alt="Logo" style={{ width: '50px', marginRight: '20px' }} /> {/* Add the logo image */}
-                <ul style={{ listStyleType: 'none', margin: 0, padding: 0, display: 'flex', alignItems: 'center' }}>
-                    <li><Link to="/" style={{ color: 'white', textDecoration: 'none', marginRight: '20px' }}>Home</Link></li>
-                    <li><Link to="/orders" style={{ color: 'white', textDecoration: 'none', marginRight: '20px' }}>Orders</Link></li>
-                </ul>
+                <Link to="/">
+                    <img src={logo} alt="Logo" style={{ width: '50px', marginRight: '20px' }} /> {/* Make the logo a link */}
+                </Link>
             </div>
             <ul style={{ listStyleType: 'none', margin: 0, padding: 0, display: 'flex', alignItems: 'center' }}>
+                <li><Link to="/orders" style={{ color: 'white', textDecoration: 'none', marginRight: '20px' }}>Orders</Link></li>
                 <li style={{ marginLeft: 'auto' }}><Link to="/checkout" style={{ color: 'white', textDecoration: 'none' }}>🛒 {cartItemCount > 0 && `(${cartItemCount})`}</Link></li>
             </ul>
         </nav>
