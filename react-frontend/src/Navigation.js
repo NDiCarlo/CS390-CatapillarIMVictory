9e0ffb265bb308aaf736ba9b4b7f61a693168dba
eriksive2 <eriksive65@gmail.com>
2024-04-23 05:17:53 -0500

    updated nav bar
    added link to a potential login page
    updated cart appearance

--- react-frontend/src/Navigation.js
+++ react-frontend/src/Navigation.js
@@ -1,5 +1,6 @@
 // Navigation.js
 import React from 'react';
+import CartDropdown from './CartDropdown.js';
 import { Link } from 'react-router-dom';
 import logo from './CATLogo.png'; // Import the logo image
 
@@ -12,11 +13,13 @@ function Navigation({ cartItemCount }) {
                 </Link>
             </div>
             <ul style={{ listStyleType: 'none', margin: 0, padding: 0, display: 'flex', alignItems: 'center' }}>
+                <li><Link to="/login" style={{ color: 'black', textDecoration: 'none', marginRight: '20px' }}>Login</Link></li>
                 <li><Link to="/orders" style={{ color: 'black', textDecoration: 'none', marginRight: '20px' }}>Orders</Link></li>
-                <li style={{ marginLeft: 'auto' }}><Link to="/checkout" style={{ color: 'white', textDecoration: 'none' }}>🛒 {cartItemCount > 0 && `(${cartItemCount})`}</Link></li>
+                <li style={{ marginLeft: 'auto' }}><Link to='/checkout' style={{ color: 'black', textDecoration: 'none' }}>Cart 🛒 {cartItemCount > 0 && `(${cartItemCount})`}</Link></li>
             </ul>
         </nav>
     );
 }
+
 
 export default Navigation;
